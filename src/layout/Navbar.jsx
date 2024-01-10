eaa2ee6d432111523cd8c5801a7bf4b538cbf9cf
developerjmanaois <128847499+developerjmanaois@users.noreply.github.com>
2024-01-10 13:38:09 +0100

    update main

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -7,29 +7,85 @@ const Navbar = () => {
 
 
         <nav>
-            <div>
-                <div>
-                    <a href="#">LOGO A/S</a>
+            <div className="navbar bg-blue-200 p-10 rounded-md">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <li><a>Item 1</a></li>
+                        <li>
+                        <a>Parent</a>
+                        <ul className="p-2">
+                            <li><a>Submenu 1</a></li>
+                            <li><a>Submenu 2</a></li>
+                        </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                    </div>
+                    <a className="btn btn-ghost text-xl">LOGO</a>
                 </div>
-
-                <div>
-                    <NavLink to="/">
-                        Home
-                    </NavLink>
-                    <NavLink to="/about">
-                        Om os
-                    </NavLink>
-                    <NavLink to="/news">
-                        Nyheder
-                    </NavLink>
-                    <NavLink to="/contact">
-                        Kontakt
-                    </NavLink>
-                    <NavLink to="/admin">
-                        ADMIN
-                    </NavLink>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-3">
+                        <li>
+                            <NavLink to="/" className='hover:text-gray-200'>
+                                Home
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about" className='hover:text-gray-200'>
+                                Om os
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/news" className='hover:text-gray-200'>
+                                Nyheder
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact" className='hover:text-gray-200' >
+                                Kontakt
+                            </NavLink>
+                        </li>
+                        <li>
+                            <details>
+                            <summary>JSONPlaceholder</summary>
+                            <ul className="p-2">
+                                <li>
+                                    <NavLink to="/postadmin" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                                PostAdmin
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/todos" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                                Todos
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/photos" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                                Photos
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/posts" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                                Posts
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to="/post" className="block px-4 py-2 text-gray-800 hover:bg-gray-200">
+                                                Post
+                                    </NavLink>
+                                </li>
+                            </ul>
+                            </details>
+                        </li>
+                    </ul>
                 </div>
-
+                <div className="navbar-end">
+                    <a className="btn">Button</a>
+                </div>
             </div>
         </nav>
     )
